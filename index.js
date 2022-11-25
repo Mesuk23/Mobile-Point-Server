c5fa986925994ee872f41129d0b6312d61bd4daa
Mesuk23 <hadeulislammesuk23@gmail.com>
2022-11-25 23:10:13 +0600

    mongodb added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
@@ -8,6 +9,20 @@ require('dotenv').config()
 app.use(cors());
 app.use(express.json())
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xfjdabp.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(err => console.error(err));
 app.get('/', (req, res) => {
     res.send('API is running')
 })
